4b2b934c59dc6f536cb5f40cf7f2d6ddd6cc54e2
gersondani2001 <gersonboche01@gmail.com>
2019-06-19 17:58:11 -0600

    app bioapp

---
+++ BioApp/www/js/controllers.js
@@ -0,0 +1,80 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+  $scope.ListaE=[{
+    title:"Transporte Activo. ",
+    p1:"El término hace referencia al movimiento de las moléculas a través de una membrana celular desde una región de baja concentración a una región de alta concentración, o en dirección opuesta a algún gradiente o a algún otro factor obstructivo (con frecuencia en la dirección opuesta al gradiente de concentración). A diferencia del transporte pasivo, que utiliza la energía cinética y la entropía natural de las moléculas moviéndose a favor de un gradiente, el transporte activo utiliza energía de las células para moverlas contra un gradiente, repulsión polar o algún otro tipo de resistencia.",
+    p2:"Algunos ejemplos de transporte activo son la captación de glucosa en los intestinos humanos y la captación de iones minerales en las células de los pelos de las raíces de las plantas.",
+    p3:"Las proteínas transmembrana especializadas reconocen a una sustancia química específica y le permiten moverse atravesando la membrana cuando, de otra forma, no podría hacerlo; ya sea porque la bicapa lipídica de la membrana es impermeable a la sustancia, o porque la sustancia estaría moviéndose en contra de su gradiente de concentración.",
+    img1:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Scheme_sodium-potassium_pump-es.svg/800px-Scheme_sodium-potassium_pump-es.svg.png",
+    img2:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Scheme_secundary_active_transport-en.svg/800px-Scheme_secundary_active_transport-en.svg.png",
+    img3:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Porters.PNG"
+    
+  },{
+    title:"Transporte Pasivo.",
+    p1:"El transporte pasivo es un movimiento de sustancias bioquímicas, atómicas o moleculares a través de membranas de la célula sin necesidad de sobrecarga de energía. A diferencia del transporte activo, no requiere una entrada de energía celular, porque es un cambio conducido por el crecimiento de la entropía del sistema. ",
+    p2:"El índice de transporte pasivo depende de la permeabilidad de la membrana de la célula, la cual, depende de la organización y características de los lípidos y proteínas de la membrana. Las cuatro clases principales de transporte pasivo son difusión simple, difusión facilitada, filtración y ósmosis.",
+    p3:"La difusión es el movimiento neto de material sólido, de un área de concentración alta a un área con concentración más baja. La diferencia de la concentración entre las dos áreas es a menudo denominada como gradientes de concentración, y la difusión continuará hasta que este gradiente haya sido eliminado. Desde que la difusión mueve materiales de un área de concentración más alta a otra más baja, está descrito como moviendo solutos a favor del gradiente de concentración (el transporte activo, mueve material de un área de concentración baja a otra área de concentración más alta, y por tanto es referido como moviendo el material contra el gradiente de concentración). Difusión simple y ósmosis son similares. La difusión simple es el movimiento pasivo de soluto, de una zona de concentración alta a una de concentración más baja, hasta que la concentración del soluto es uniforme en todas partes y alcanza el equilibrio. En cambio, la ósmosis específicamente describe el movimiento del agua (no del soluto) a través de una membrana hasta que hay una concentración igual de agua y soluto en ambos lados de la membrana. Difusión simple y ósmosis son formas de transporte pasivo y no requieren de la energía de ATP de la célula.",
+    img1:"http://2.bp.blogspot.com/-16kcCx9lVO0/UIhpiVoWdpI/AAAAAAAAAE8/DYSs80e-5cQ/s1600/Dibujo%25203%2520nuevo%5B1%5D.jpg",
+    img2:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Blausen_0394_Facilitated_Diffusion.png/320px-Blausen_0394_Facilitated_Diffusion.png",
+    img3:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Filtration_diagram.svg/320px-Filtration_diagram.svg.png"
+    
+  },{
+    title:"Sintesis de fosfageno.",
+    p1:"El fosgeno (COCl2), del griego: φωσ (phos): luz y γένος-ου (genos):generador, es un componente químico industrial utilizado para hacer plásticos y pesticidas. A temperatura ambiente (21 °C), el fosgeno es un gas venenoso. Si es enfriado y presurizado, el gas de fosgeno puede ser convertido en líquido, de forma que pueda ser transportado y almacenado. Cuando se libera fosgeno líquido, éste se transforma rápidamente en gas que permanece cerca del suelo y se propaga con rapidez (es más denso que el aire y por esa razón se expande hacia áreas más bajas). Al fosgeno también se le conoce por su denominación militar CG.",
+    p2:"El fosgeno, por sí mismo, no es inflamable (no se enciende ni se quema con facilidad) pero es un comburente (puede causar que prendan las sustancias inflamables que hay a su alrededor).",
+    p3:"El fosgeno u oxicloruro de carbono, cuya fórmula química es COCl2, es un gas generalmente incoloro y no inflamable, con un olor agradable, similar al del heno recién cortado. Es una sustancia química artificial, aunque pequeñas cantidades son formadas en la naturaleza a partir de la degradación de compuestos del cloro.",
+    img1:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Phosgen.svg/320px-Phosgen.svg.png",
+    img2:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Phosgene-3D-vdW.png/320px-Phosgene-3D-vdW.png",
+    img3:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Phosgen.svg/320px-Phosgen.svg.png"
+  },{
+    title:"Osmosis y Difusion.",
+    p1:"La ósmosis u osmosis (del griego ὠσμός ōsmós 'acción de empujar', 'impulso' y -sis)1​ es un fenómeno físico relacionado con el movimiento de un disolvente a través de una membrana semipermeable. Tal comportamiento supone una difusión simple a través de la membrana, sin gasto de energía. La ósmosis del agua es un fenómeno biológico importante para el metabolismo celular de los seres vivos. Tipos.",
+    p2:"Se denomina membrana semipermeable a aquella estructura que contiene poros o agujeros, al igual que cualquier filtro, de tamaño molecular. El tamaño de los poros es tan minúsculo que deja pasar las moléculas pequeñas pero no las grandes, normalmente del tamaño de micrómetros. Por ejemplo, deja pasar las moléculas de agua, que son pequeñas, pero no las de azúcar, que son más grandes. Si una membrana como la descrita separa un líquido en dos particiones, una de ... y difusión simple , entendiendo que este último fenómeno implica no sólo el movimiento al azar de las partículas hasta lograr la homogénea distribución de las mismas y esto ocurre cuando las partículas que vienen se equiparan con las que aleatoriamente van, sino el equilibrio de los potenciales químicos de ambas particiones.",
+    p3:"Experimento ilustrativo de ósmosis Se puede realizar una experiencia, consistente en llenar una bolsa de celofán (que también se usa para bolsas de supermercado), con una solución de agua y azúcar común; la boca de la bolsa debe quedar herméticamente cerrada. En estas condiciones, se la sumerge en un recipiente que contenga agua. El celofán cumple el papel de una membrana y la característica que presenta es la de no permitir el paso de las moléculas de azúcar en la solución, lo cual significa que es impermeable al azúcar. Por el contrario, deja pasar con facilidad las moléculas de agua, o sea, es permeable a ella. Las membranas que presentan este comportamiento reciben el nombre de semipermeables. De acuerdo a lo anterior, en el experimento se puede observar que, al comienzo el recipiente formado por la membrana de celofán se encuentra distendido, pero poco a poco va aumentando de tamaño hasta quedar inflado. Esto es consecuencia de la difusión del agua a través de la membrana, desde el recipiente hacia el interior de la bolsa. Este es el fenómeno que se conoce como ósmosis.",
+    img1:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Osmose2-es.png/320px-Osmose2-es.png",
+    img2:"https://upload.wikimedia.org/wikipedia/commons/7/76/%C3%96smosis-inversa.png",
+    img3:"http://100cia.site/images/4912.jpg"
+  },{
+    title:"Transporte de energia en el cuerpo(celular).",
+    p1:"El transporte celular es el intercambio de sustancias a través de la membrana plasmática, que es una membrana semipermeable.",
+    p2:"El transporte es importante para la célula porque le permite expulsar de su interior los desechos del metabolismo, también el movimiento de sustancias que sintetiza como hormonas. Además es la forma en que adquiere nutrientes mediante procesos de incorporación a la célula de nutrientes disueltos en el agua. Las vías de transporte a través de la membrana celular y los mecanismos básicos para las moléculas de pequeños tamaños.",
+    p3:"La ósmosis es un tipo de transporte pasivo en el cual solo las moléculas de agua son transportadas a través de la membrana. El movimiento se realiza a favor de la gradiente, esto es desde el medio de mayor concentración de agua (menor concentración de soluto) hacia al de menor concentración de agua (mayor concentración de soluto y solvente), con ello permite equilibrar las concentraciones del soluto de los medios separados por la membrana celular. La función de la ósmosis es mantener hidratada a la célula, dicho proceso no requiere del gasto de energía (ATP).",
+    img1:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Osmotic_pressure_on_blood_cells_diagram-es.svg/800px-Osmotic_pressure_on_blood_cells_diagram-es.svg.png",
+    img2:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Turgor_pressure_on_plant_cells_diagram-es.svg/800px-Turgor_pressure_on_plant_cells_diagram-es.svg.png",
+    img3:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Tipos_de_endocitosis.svg/800px-Tipos_de_endocitosis.svg.png"
+  },{
+    title:"Membrana plasmatica.",
+    p1:"La membrana plasmática, membrana celular, membrana citoplasmática o plasmalema, es una bicapa lipídica que delimita toda la célula.1​ Es una estructura formada por dos láminas de fosfolípidos, glucolípidos y proteínas que rodean, limitan la forma y contribuyen a mantener el equilibrio entre el interior (medio intracelular) y el exterior (medio extracelular) de las células. Regula la entrada y salida de muchas sustancias entre el citoplasma y el medio extracelular.2​ Es similar a las membranas que delimitan los orgánulos de células eucariotas.",
+    p2:"Está compuesta por dos láminas que sirven de contenedor para el citosol y los distintos compartimentos internos de la célula, así como también otorga protección mecánica. Está formada principalmente por fosfolípidos (fosfatidiletanolamina y fosfatidilcolina), colesterol, glúcidos y proteínas (integrales y periféricas).",
+    p3:"La principal característica de esta barrera es su permeabilidad selectiva, lo que le permite seleccionar las moléculas que deben entrar y salir de la célula. De esta forma se mantiene estable el medio intracelular, regulando el paso de agua, iones y metabolitos, a la vez que mantiene el potencial electroquímico (haciendo que el medio interno esté cargado negativamente). La membrana plasmática es capaz de recibir señales que permiten el ingreso de partículas a su interior.",
+    img1:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Detalle_de_la_membrana_celular.svg/800px-Detalle_de_la_membrana_celular.svg.png",
+    img2:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Cell_membrane_drawing-es.svg/1024px-Cell_membrane_drawing-es.svg.png",
+    img3:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Fluid_Mosaic.svg/1280px-Fluid_Mosaic.svg.png"
+  }]
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
